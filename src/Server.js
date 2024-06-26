ab7089beba7f29b163fbd1226073ec58fb83fe38
Phung Duc Hung <phungduchung9808@gmail.com>
2024-06-26 15:49:03 +0700

    Set up Routes

--- src/Server.js
+++ src/Server.js
@@ -1,31 +1,19 @@
 // Import the Express module
 const express = require('express');
-const path = require('path');
 require('dotenv').config() // .evn
-
-// Create an instance of an Express application
-const app = express();
+const configViewEngine = require('./config/viewEngine')
+const app = express(); // Create an instance of an Express application
+const webRoutes = require('./routes/web');
 // Define the port number where the server will listen for requests
 const port = process.env.PORT || 8888 ;  // 8888: nếu undefined server sẽ ko die
 const hostname = process.env.HOST_NAME;
 
 
 // config template engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// config static files
-app.use (express.static (path.join ( __dirname , 'public')))
+configViewEngine(app)
 
 // khai báo route -- get is a method
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.get('/abc', (req, res) => {
-  // res.send('<h1> Hello Michael! </h1>');
-  res.render("sample.ejs")
-});
+app.use('/',webRoutes); // sau dấu / để chia version route
 
 
 // Start the server and have it listen on the specified port
