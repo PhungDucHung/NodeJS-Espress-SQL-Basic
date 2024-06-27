17e4b2861a4087b08752604e8eb2fe8a8bcc760a
Phung Duc Hung <phungduchung9808@gmail.com>
2024-06-28 00:48:53 +0700

    Design NavBar and Form Add New

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,19 +2,7 @@ const connection = require('../config/database')
 
 
 const getHomepage = (req, res) => {
-    // process data
-    // call , model
-    // simple query
-connection.query(
-    'select * from users u ',
-    function (err, results, fields) {
-        users = results;
-        console.log(">>> results = ",results); // results contains rows returned by server
-        res.send(JSON.stringify(users)); // Câu lệnh này sẽ tự động chuyển đổi đối tượng JavaScript thành JSON
-            // res.send(users); 
-
-     }
-  );
+    return  res.render("home.ejs")
 }
 
 const getABC = (req, res) => {
