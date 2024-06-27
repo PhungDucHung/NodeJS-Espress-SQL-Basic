51c07f256df83ec3dc080654cdd7ea6a362a16ed
Phung Duc Hung <phungduchung9808@gmail.com>
2024-06-27 23:48:27 +0700

    Connection Pool Pattern

--- src/Server.js
+++ src/Server.js
@@ -4,7 +4,8 @@ require('dotenv').config() // .evn
 const configViewEngine = require('./config/viewEngine')
 const app = express(); // Create an instance of an Express application
 const webRoutes = require('./routes/web');
-const mysql = require('mysql2');
+const connection = require('./config/database')
+
 // Define the port number where the server will listen for requests
 const port = process.env.PORT || 8888 ;  // 8888: nếu undefined server sẽ ko die
 const hostname = process.env.HOST_NAME;
@@ -17,23 +18,7 @@ configViewEngine(app)
 app.use('/',webRoutes); // sau dấu / để chia version route
 
 //test connection
-// Create the connection to database
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'nodeJs',
-});
-
-connection.query(
-  'select * from users u ',
-  function (err, results, fields) {
-    console.log(">>> results = ",results); // results contains rows returned by server
-    console.log(">>> fields = ",fields); // fields contains extra meta data about results, if available
-  }
-);
 
 app.listen(port,hostname, () => {
   console.log(`Example app listening on port ${port}`);
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,20 @@
+const connection = require('../config/database') 
+
+
 const getHomepage = (req, res) => {
     // process data
     // call , model
-    res.send('hello mình là wibu')
+    // simple query
+connection.query(
+    'select * from users u ',
+    function (err, results, fields) {
+        users = results;
+        console.log(">>> results = ",results); // results contains rows returned by server
+        res.send(JSON.stringify(users)); // Câu lệnh này sẽ tự động chuyển đổi đối tượng JavaScript thành JSON
+            // res.send(users); 
+
+     }
+  );
 }
 
 const getABC = (req, res) => {
