e993a64fb8ed5adc0e6e7faf1d08b5a9963d0fd1
Phung Duc Hung <phungduchung9808@gmail.com>
2024-06-28 17:43:48 +0700

    Convert Callback to Async Database and Dessign List Users

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -8,24 +8,39 @@ const getHomepage = (req, res) => {
 const getABC = (req, res) => {
     res.render("sample.ejs")
 }
+
+const getCreatePage = (req, res) => {
+    res.render("create.ejs")
+}
  
-const postCreateUser = (req, res) => {
+const postCreateUser =  async (req, res) => {
     let email = req.body.email;
     let myName = req.body.name;
     let city = req.body.city;
     // let {email, myName, city} = req.body
 
-    connection.query(
-        `INSERT INTO Users (email , name , city )
-        VALUES (?, ?, ?)`,
-        [email , myName , city],
-        function (err, results) {
-          console.log(results);
-          res.end('Create user successfully')
-        }
+    // ----------CALL BACK ----------
+    // connection.query(  // truyền động giá trị
+    //     `INSERT INTO Users (email , name , city )VALUES (?, ?, ?)`, [email , myName , city],
+    //     function (err, results) {
+    //       console.log(results);
+    //       res.send('Create user successfully')
+    //     }
+    //   );
+        
+    //---------ASYNC / AWAIT----------
+    let [results,fields] = await  connection.query(  // truyền động giá trị
+        `INSERT INTO Users (email , name , city ) VALUES (?, ?, ?)`,[email , myName , city],
       );
+      console.log(">>> Check Result :" ,results);
+      res.send('Create user successfully')
+
+
+    // const [results , fields] = await connection.query('SELECT * FROM Users u');
 }
 
+
+
 module.exports = {
-    getHomepage , getABC , postCreateUser
+    getHomepage , getABC , postCreateUser ,getCreatePage
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getHomepage , getABC ,postCreateUser} = require('../controllers/homeController');
+const { getHomepage, getABC, postCreateUser, getCreatePage } = require('../controllers/homeController');
 
-router.get('/', getHomepage); 
-  
+router.get('/', getHomepage);
 router.get('/abc', getABC);
 
+// Corrected the route for getCreatePage
+router.get('/create', getCreatePage);
+
+// Corrected the route for postCreateUser
 router.post('/create-user', postCreateUser);
 
 module.exports = router;
